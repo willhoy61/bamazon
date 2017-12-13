ee78ef9ba209b0324ed77e44f0007dadba1f2c71
William Hoy <willi@DESKTOP-8JUUS67>
2017-12-12 23:30:08 -0600

    working on resoponses

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var inquire = require ("inquirer");
 var mysql = require ("mysql");
 
+'use strict';
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -17,15 +19,49 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   readProducts();
+  start();
   
 });
+function start() {
+  inquirer.prompt([
+    {
+    name: "item",
+    type: "input",
+    message: "What is the id of the item you would like to purchase?"
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "How many units would you like to purchase?"
+    }
+  ]).then(function(answer) {
+
+    connection.query(
+      "INSERT INTO products SET ?",
+      {
+        item_id: answer.item,
+        quantity: answer.quantity,
+      },
+  function(err) {
+  if(err) throw err;
+  console.log("your purchase was successfull");
+  readProducts();
+  start();
+      }
+    );
+  });
+}
+
+
+
 function readProducts() {
   console.log("Selecting all products...\n");
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.log(res);
-    connection.end();
+    
   });
 }
 
+connection.end();

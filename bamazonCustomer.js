700708b7c560400dd50dbe2374f41e7672d9d05b
William Hoy <willi@DESKTOP-8JUUS67>
2017-12-12 20:55:52 -0600

    reading input

---
+++ bamazonCustomer.js
@@ -0,0 +1,31 @@
+var inquire = require ("inquirer");
+var mysql = require ("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon_DB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
+  
+});
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+

8205f7b99b606a4d79e1fce8cd6aace6dcac55ec
willhoy61 <willhoy61@gmail.com>
2017-12-15 16:00:37 -0600

    updates database removed print all function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,6 @@ var mysql = require ("mysql");
 var inquirer = require ("inquirer");
 
 
-'use strict';
 // connect to mysql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -21,17 +20,8 @@ connection.connect(function(err) {
     throw err
   console.log("connected as id " + connection.threadId + "\n");
 });
-start();
 
-// inquirer function asks user what they would like to purchase and how many
-function start() {
-readProducts();
-  var id;
-  var quanUP = 0;
-  connection.query("SELECT * FROM products", function(err, results) {
-    if (err) throw err;
-  inquirer
-  .prompt([
+var custQ = [
   {
     name:"item",
     type: "input",
@@ -42,106 +32,35 @@ readProducts();
     message: "How many would you like to purchase?"
   }
   //answer takes user input and returns it to item_id user is purchasing
-]).then(function(answer) {
-  for(var i=0; i<results.length; i++) {
-    if(results[i].answer == answer.item) {
-      id = results[i];
-        //if validates that there is enough quantity for user to purchase
-        if(results[i].quantity < id.quantity) {
-          console.log("we do not have any more in stock");
-          start();
-        } else if(results[i].quantity >= id.quantity) {
-          price(id.quantity, id.price);
-          purchase(results[i].quantity, id.quantity, id.item);
-        }
-        updateDb(quanUp);
-    }
-  }
-})
-})
-};
-//displays the price based on how many the user ordered
-function price(pur, pri) {
-  var total = pur * pri;
-  console.log("each item cost $ " + p);
-  console.log("Your Total is : $ " + total);
-}
-// updates the data base with depleted inventory
-function updateDb(q, p, r) {
-  quanUp = q - p;
-  connection.query("UPDATE products SET ? where ?", [{quantity: quanUp}, {item: r}],
-function (err) {
-  if (err) throw err;
-  console.log("purchase Complete");
-})
-  connection.end();
-};
-// validates users purchase and makes sure they want to proceed
-function purchase (q, p, r) {
-  inquirer.prompt([{
-    name: "yesorno",
-    type: "rawlist",
-    message:"Would you like to complete this purchase?",
-    choices: ["yes", "no"]
-  }]).then(function(answer){
-    if(answer.yesorno.toLowerCase() === "yes") {
-      quanUp(q, p, r);
-    } else {
-      start();
-    }
-  })
-}
+];
+start();
 
+// inquirer function asks user what they would like to purchase and how many
+function start() {
+//readProducts();
+  inquirer
+  .prompt(custQ).then(function(answer) {
 
-// shows user inventory available to purchase
-function readProducts() {
-  console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-      //start();
-     });
-}
-
-//function start() {
-//   inquirer.prompt(
-//     {
-//     name: "item",
-//     type: "input",
-//     message: "What is the id of the item you would like to purchase?"
-//     },
-//     {
-//       name: "quantity",
-//       type: "input",
-//       message: "How many units would you like to purchase?",
-//       validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//     }
-//   ).then(function(answer) {
-//     //var query = "SELECT  item_id, quantity FROM products WHERE quantity item_id: answer.item";
-
-//     connection.query(
+    var item = answer.item;
+    var quant = parseInt(answer.quantity);
+    var query = "SELECT * FROM products WHERE ?";
+    connection.query(query, {item_id: item}, function (err, results) {
       
-//       )
-//     connection.query(
-//       "UPDATE products SET ? WHERE ?",
-//       [
-//       {
-//         quantity: answer.quantity - products.quantity
-//       },
-//       {
-//         item_id: answer.item
-//       }
-//       ],
-//       function(err) {
-//           if (err) throw err;
-//           console.log("Your purchase was Successful!");
-//         }
-//       );
-//   });
-// }
+       var inStock = results[0].quantity;
+
+       var total = results[0].price * quant;
+
+       if(quant > inStock){
+        console.log("we do not have enough inventory");
+        } else {
+            inStock -= quant;
+            query = "UPDATE products SET ? WHERE ?";
+            connection.query(query, [{quantity: inStock}, {item_id: item}], function(err, results) {
+            });
+            console.log("the total of your purchase is : " + total);
+        }
+
+    });
+  });
+}
+  
